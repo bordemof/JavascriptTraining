43573acd0b2d4a7f574599829596e3245d27831a
bordemof <lonami80@gmail.com>
2013-12-25 09:02:05 +0100

    Added update samples

---
+++ Objects/Update.js
@@ -0,0 +1,9 @@
+stooge['first-name'] = "Forrest";
+stooge['middle-name'] = "Jebedhia";
+stooge.nickname = 'RunningF';
+
+flight.equipment = {
+	model	: 'Boeing 737'
+};
+flight.status = 'overdue';
+
