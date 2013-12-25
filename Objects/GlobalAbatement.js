23c8683093d09898a9d1ac1ad76890991470a41b
bordemof <lonami80@gmail.com>
2013-12-25 09:51:02 +0100

    Added GlobalAbatement sample

---
+++ Objects/GlobalAbatement.js
@@ -0,0 +1,22 @@
+var MYAPP = {};
+
+MYAPP.stooge = {
+	"first-name": "Harry",
+	"last-name"	: "Potter"
+};
+
+MYAPP.flight = {
+	airline		: "Ocenaic",
+	number 		: "815",
+	departure	: {
+		IATA	: "SYD",
+		time 	: "2004-09-23 10:42",
+		city	: "Sydney",
+	},
+	arrival: {
+		IATA	: "LAX",
+		time	: "2004-09-22 14:55",
+		city	: "Los Angeles"
+	}
+};
+
