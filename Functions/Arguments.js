f74ca283996631ba51025d7a56be71505b9f20ed
bordemof <lonami80@gmail.com>
2013-12-25 10:29:08 +0100

    Added Arguments sample

---
+++ Functions/Arguments.js
@@ -0,0 +1,9 @@
+var sum = function () {
+	var i, sum = 0;
+	for (i=0; i < arguments.length; i += 1) {
+		sum += arguments[i];
+	}
+	return sum;
+};
+
+document.writeln(sum(4, 8, 15, 16, 23, 42)); //108
