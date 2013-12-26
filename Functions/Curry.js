9a578d099d04180751f5fc8f78babf0cf03ec141
bordemof <lonami80@gmail.com>
2013-12-26 15:18:55 +0100

    Added Curry sample

---
+++ Functions/Curry.js
@@ -0,0 +1,18 @@
+var add1 = add.curry(1);
+document.writeln(add1(6)); //7
+
+Function.method('curry', function () {
+	var args = arguments, that = this;
+	return function () {
+		return that.apply(null, args.concat(arguments));
+	};
+});
+
+Function.method('curry', function () {
+	var slice = Array.prototype.slice,
+		args = slice.apply(arguments),
+		that = this;
+	return function () {
+		return that.apply(null, args.concat(slice.apply(arguments)));
+	};
+});
